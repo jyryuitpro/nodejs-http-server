cec403d01cb60bbe41ae3ca8f0820c3fe384622b
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-18 20:24:50 +0900

    HTTP 서버 만들기

---
+++ server1.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.write('<h1>Hello Node!</h1>');
+    res.write('<p>Hello server</p>');
+    res.end('<p>Hello jyryuitpro</p>');
+})
+    // .listen(8080, () => {
+    //     console.log('8080번 포트에서 서버 대기 중입니다.');
+    // });
+
+    .listen(8080);
+server.on('listening', () => {
+    console.log('8080번 포트에서 서버 대기 중입니다.');
+});
+server.on('listening', (error) => {
+    console.error(error);
+});
