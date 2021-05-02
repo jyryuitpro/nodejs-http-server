3fd4286c14316f68bf525487e85dd719c6b95d77
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-05-02 22:36:35 +0900

    fs로 HTML 읽어 제공하기

--- 04.http 모듈로 서버 만들기/01.HTTP 서버 만들기.js
+++ 04.http 모듈로 서버 만들기/01.HTTP 서버 만들기.js
@@ -1,16 +1,23 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
     res.write('<h1>Hello Node!</h1>');
     res.write('<p>Hello server</p>');
     res.end('<p>Hello jyryuitpro</p>');
 })
     .listen(8080);
-
 server.on('listening', () => {
     console.log('8080번 포트에서 서버 대기 중입니다.');
 });
-
 server.on('error', (error) => {
     console.error('error');
 });
+
+const server1 = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.write('<h1>Hello Node!</h1>');
+    res.write('<p>Hello server</p>');
+    res.end('<p>Hello jyryuitpro</p>');
+})
+    .listen(8081);
